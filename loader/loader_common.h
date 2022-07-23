114e3c88c09ff5818726443c880852a6505a4ab7
Aaditya Chandrasekhar <aadityac@gmail.com>
2022-07-23 19:20:53 +0000

    Move vk_loader_platform.h to top of import lists as windows targets require winsock2.h to be imported before windows.h.

--- loader/loader_common.h
+++ loader/loader_common.h
@@ -29,13 +29,13 @@
 
 #pragma once
 
+#include "vk_loader_platform.h"
 #include "vulkan/vk_platform.h"
 #include "vulkan/vk_sdk_platform.h"
 #include <vulkan/vulkan.h>
 #include <vulkan/vk_layer.h>
 #include <vulkan/vk_icd.h>
 
-#include "vk_loader_platform.h"
 #include "vk_loader_layer.h"
 #include "vk_layer_dispatch_table.h"
 #include "vk_loader_extensions.h"
